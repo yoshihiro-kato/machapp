079aab60f8bd75cc0ce2d0e041b44e3aaaff9dcf
yoshihiro-kato <12379330+yoshihiro-kato@users.noreply.github.com>
2026-09-21 11:16:17 +0900

    Add requirements and design

--- sample.js
+++
@@ -1,8 +0,0 @@
-
-function calculateDaysBetweenDates(begin, end) {
-    var beginDate = new Date(begin);
-    var endDate = new Date(end);
-    var timeDiff = Math.abs(endDate.getTime() - beginDate.getTime());
-    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return diffDays;
-} 
